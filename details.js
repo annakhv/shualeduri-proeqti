06b783fcd8c0625dd9cf17315209bd3512b169ad
annakhv <annakhvedelidze@gmail.com>
2021-02-17 18:21:23 +0400

    commit

--- details.js
+++ details.js
@@ -1,16 +1,18 @@
 var dataNum=localStorage.getItem("clickedCountry")
 var data=JSON.parse(localStorage.getItem("data"))
-console.log(dataNum);
-console.log(data)
+
 
 writeCountryDetails(dataNum);
 
 
 
 function writeCountryDetails(datanum) {
-            console.log( data[datanum])
            document.querySelector("#country").innerHTML=data[datanum]['name'];
             document.querySelector("#title0").innerHTML="general information"
+            flag=document.createElement('img');
+            flag.setAttribute('src', data[datanum]['flag']);
+            flag.setAttribute("height", "300px")
+            document.querySelector("#flag").append(flag)
            addCountryData("capital city:", data[datanum]['capital'], "det");
            addCountryData("population number:", data[datanum]['population'], "det");
            addCountryData("area:", data[datanum]['area'], "det");
@@ -20,18 +22,22 @@ function writeCountryDetails(datanum) {
            const borders=data[datanum]['borders'];
            addCountryDataList("country borders", borders, "det1", "title");
            const translations=data[datanum]['translations'];
-           console.log(translations)
            addCountryDataList("translations", translations, "det0", "title1");
-           currency=data[datanum]["currencies"]['0'];
-           console.log(currency)
-           addCountryDataList("currencies", currency, "det3", "title3");
-           languages=data[datanum]['languages']['0'];
-           console.log(data[datanum]["languages"]);
-           addCountryDataList("languages", languages, "det2", "title2");
-           blocks=data[datanum]['regionalBlocs']['0'];
-           console.log(data[datanum]["regionalBlocs"]);
-           addCountryDataList("Regional Blocks", blocks, "det4", "title4");
-           
+           currencyList=data[datanum]["currencies"];
+           for(currency in currencyList){
+             addCountryDataList("currencies", currencyList[currency], "det3", "title3");
+           }
+       
+           languagesList=data[datanum]['languages'];
+           for (languages in languagesList){
+             addCountryDataList("languages", languagesList[languages], "det2", "title2");
+           }
+           blocksLists=data[datanum]['regionalBlocs'];
+           for (block in blocksLists){
+               addCountryDataList("Regional Blocks", blocksLists[block], "det4", "title4");
+           }
+          
+         
 }
 
 
--- dist/details.dev.js
+++ dist/details.dev.js
@@ -2,14 +2,15 @@
 
 var dataNum = localStorage.getItem("clickedCountry");
 var data = JSON.parse(localStorage.getItem("data"));
-console.log(dataNum);
-console.log(data);
 writeCountryDetails(dataNum);
 
 function writeCountryDetails(datanum) {
-  console.log(data[datanum]);
   document.querySelector("#country").innerHTML = data[datanum]['name'];
   document.querySelector("#title0").innerHTML = "general information";
+  flag = document.createElement('img');
+  flag.setAttribute('src', data[datanum]['flag']);
+  flag.setAttribute("height", "300px");
+  document.querySelector("#flag").append(flag);
   addCountryData("capital city:", data[datanum]['capital'], "det");
   addCountryData("population number:", data[datanum]['population'], "det");
   addCountryData("area:", data[datanum]['area'], "det");
@@ -19,17 +20,24 @@ function writeCountryDetails(datanum) {
   var borders = data[datanum]['borders'];
   addCountryDataList("country borders", borders, "det1", "title");
   var translations = data[datanum]['translations'];
-  console.log(translations);
   addCountryDataList("translations", translations, "det0", "title1");
-  currency = data[datanum]["currencies"]['0'];
-  console.log(currency);
-  addCountryDataList("currencies", currency, "det3", "title3");
-  languages = data[datanum]['languages']['0'];
-  console.log(data[datanum]["languages"]);
-  addCountryDataList("languages", languages, "det2", "title2");
-  blocks = data[datanum]['regionalBlocs']['0'];
-  console.log(data[datanum]["regionalBlocs"]);
-  addCountryDataList("Regional Blocks", blocks, "det4", "title4");
+  currencyList = data[datanum]["currencies"];
+
+  for (currency in currencyList) {
+    addCountryDataList("currencies", currencyList[currency], "det3", "title3");
+  }
+
+  languagesList = data[datanum]['languages'];
+
+  for (languages in languagesList) {
+    addCountryDataList("languages", languagesList[languages], "det2", "title2");
+  }
+
+  blocksLists = data[datanum]['regionalBlocs'];
+
+  for (block in blocksLists) {
+    addCountryDataList("Regional Blocks", blocksLists[block], "det4", "title4");
+  }
 }
 
 function addCountryData(description, inputvar, place) {
